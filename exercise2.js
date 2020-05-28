e4333bbcc05610e58ae6efed23c6cf8759575b1a
Kevin Keesee <64923372+kevinkeesee@users.noreply.github.com>
2020-05-28 11:40:22 -0500

    Finished exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -10,8 +10,11 @@ for(let i=0; i<10; i++) {
 
 
 //your code...
-
-
+let i = 0;
+while(i < 10){
+    console.log(" the value of i in the loop is : " + i);
+    i++;
+}
 
 
 /************************************************************* */
@@ -20,6 +23,22 @@ for(let i=0; i<10; i++) {
 //use javascript to compute the value of the above statement. Each individual operation needs to be a function expression. run all the functions after defining them, and print the answer to the console.
 
 //your code...
+function multiply(var1, var2){
+    return var1 * var2;
+}
+function add(var1, var2){
+    return var1 + var2;
+}
+function divide(var1, var2){
+    return var1 / var2;
+}
+function square(var1){
+    return var1 * var1;
+}
+let answer = add(30, 2);
+answer = multiply(answer, 20);
+answer = divide(answer, square(10))
+console.log(answer);
 
 
 /************************************************************* */
@@ -38,8 +57,78 @@ for(let i=0; i<10; i++) {
 // 125
 // undefined
 // ""
-
-
+const twenty = 20;
+const zeroInt = 0;
+const zeroString = "zero";
+const zero = 20;
+const nullValue = null;
+const zeroChar = "0";
+const emptyStringInverted = !"";
+const emptyBrackets = {};
+const helloTEKcamp = () => {console.log("hello TEKcamp!")};
+const hundredTwentyFive = 125;
+const undefinedVar = undefined;
+const emptyString = "";
+if (twenty){
+    console.log(`${twenty} is truthy because it is a nonzero integer.`);
+} else{
+    console.log(`${twenty} is falsy.`);
+}
+if (zeroInt){
+    console.log(`${zeroInt} is truthy.`);
+} else{
+    console.log(`${zeroInt} is falsy because it is zero.`);
+}
+if (zeroString){
+    console.log(`${zeroString} is truthy because it is a nonempty string.`);
+} else{
+    console.log(`${zeroString} is falsy.`);
+}
+if (zero){
+    console.log(`${zero} is truthy because it is a nonzero integer.`);
+} else{
+    console.log(`${zero} is falsy.`);
+}
+if (nullValue){
+    console.log(`${nullValue} is truthy.`);
+} else{
+    console.log(`${nullValue} is falsy because it is a null value.`);
+}
+if (zeroChar){
+    console.log(`${zeroChar} is truthy because it is a nonempty string.`);
+} else{
+    console.log(`${zeroChar} is falsy.`);
+}
+if (emptyStringInverted){
+    console.log(`${emptyStringInverted} is truthy because it is the inverse of an empty string.`);
+} else{
+    console.log(`${emptyStringInverted} is falsy.`);
+}
+if (emptyBrackets){
+    console.log(`${emptyBrackets} is truthy because it is an object.`);
+} else{
+    console.log(`${emptyBrackets} is falsy.`);
+}
+if (helloTEKcamp){
+    console.log(`${helloTEKcamp} is truthy because it is a function.`);
+} else{
+    console.log(`${helloTEKcamp} is falsy.`);
+}
+if (hundredTwentyFive){
+    console.log(`${hundredTwentyFive} is truthy because it is a nonzero integer.`);
+} else{
+    console.log(`${hundredTwentyFive} is falsy.`);
+}
+if (undefinedVar){
+    console.log(`${undefinedVar} is truthy.`);
+} else{
+    console.log(`${undefinedVar} is falsy because it is undefined.`);
+}
+if (emptyString){
+    console.log(`${emptyString} is truthy.`);
+} else{
+    console.log(`${emptyString} is falsy because it is an empty string.`);
+}
 
 
 /************************************************************* */
@@ -57,11 +146,30 @@ if(day === "monday") {
 } else if (day === "thursday") {
     console.log("Thursday night... the mood is right");
 } else if (day === "friday") {
-    console.log("TGIF.  Friday truly is the best day of the week!")
+    console.log("TGIF.  Friday truly is the best day of the week!");
 } else {
-    console.log("It's a weekend!")
+    console.log("It's a weekend!");
 }
 
+switch(day){
+    case "monday":
+        console.log("we got a long week ahead of us...");
+        break;
+    case "tuesday":
+        console.log("tuesday's are still beterr than mondays, but LONG way to go still");
+        break;
+    case "wednesday":
+        console.log("We are smack dab in the middle of the week");
+        break;
+    case "thursday":
+        console.log("Thursday night... the mood is right");
+        break;
+    case "friday":
+        console.log("TGIF.  Friday truly is the best day of the week!");
+        break;
+    default:
+        console.log("It's a weekend!");
+}
 
 
 /************************************************************* */
@@ -78,6 +186,12 @@ if (age > 13 && age < 19) console.log('teen'); else {
 if (age > 65) console.log("retired"); else {
     console.log("still working...");
 }
+
+(age > 21) ? console.log("adult") : console.log("minor");
+
+(age > 13 && age < 19) ? console.log('teen') : console.log("not a teenager");
+
+(age > 65) ? console.log("retired") : console.log("still working...");
 
 
 /************************************************************* */
@@ -96,20 +210,48 @@ if (age > 65) console.log("retired"); else {
 */
 
 //your code...
+const stupid = {
+    name: 'Kevin Keesee',
+    age: 23,
+    gender: 'Male',
+    hobbies: ["Video Games", "PC Building", "Music Theory"],
+    profession: 'Web design',
+    education: 'High school diploma',
+
+    learn: function(){
+        console.log(this.name);
+    },
+    explainProfession: function(){
+        console.log(this.profession);
+    }
+}
 
 
 /************************************************************* */
 // Problem 6: Create an object literal that represents any object in the real world that you like.  Try to come up with a realistic set of properties and methods that would accurately describe that object.  In at least one of your methods, use the this keyword to refer back to one of the properties you defined on this object literal.  
 
 //your code...
+const myPC = {
+    cpu: "Ryzen 7 3700X",
+    gpu: "RTX 2060 Super",
+    ram: "32gb 3600mhz rgb",
+    ssd: "Intel 660p 2TB",
 
+    benchmark: function(){
+        console.log("Over 9000 in Time Spy!");
+    }
+}
 
 
 /************************************************************* */
 //Problem 7: create a function that outputs your 3 favorite data types, with a message explaining why you like that data type.
 
 //your code...
-
+function favoriteDataTypes(){
+    console.log("I like the " + typeof "a string" + " because they're the reason we can code easier than Assembly.");
+    console.log("I like the " + typeof true + " because it's powerful and simple and versatile.");
+    console.log("I like the " + typeof 42 + " because math is a vital part of programming logic outside of the scope of a simple binary gate.");
+}
 
 
 /************************************************************* */
